0aa78c4abd44ff544993acc537ef8a3a37ebfb2f
Nico Nap <nico.nap@outlook.com>
2021-04-26 10:43:12 +0200

    Add Gameboard with two methods

--- scripts.js
+++ scripts.js
@@ -31,4 +31,53 @@ function Ship(length) {
     }
 }
 
-module.exports = { Ship };
+function Gameboard() {
+    // Stores all the ships, their coordinates and orientations
+    this.ships = [];
+
+    // Stores all the coordinates of missed spots
+    this.missedAttacks = [];
+
+    // Make an object with a Ship(), coordinates and orientation
+    this.placeShip = function(x, y, or, length) {
+        if (or == "v") {
+            let yco = y;
+            y = [];
+            for (let i = yco; i < yco + length; i++) {
+                y.push(i);
+            }
+        } else if (or == "h") {
+            let xco = x;
+            x = [];
+            for (let i = xco; i < xco + length; i++) {
+                x.push(i);
+            }
+        }
+        this.ships.push({
+            ship: new Ship(length),
+            x,
+            y,
+            or
+        })
+    }
+
+    // Handle attacks by determining if a ship has been hit or not and
+    // telling the ship that has been hit where it's been hit
+    this.receiveAttack = function(x, y) {
+        this.ships.forEach(element => {
+            if (element.x == x || element.y == y) {
+                if (element.or == "v") {
+                    if (element.y.includes(y)) {
+                        element.ship.hit(element.y.indexOf(y));
+                    }
+                } else if (element.or == "h") {
+                    if (element.x.includes(x)) {
+                        element.ship.hit(element.x.indexOf(x));
+                    }
+                }
+            }
+        })
+    }
+}
+
+module.exports = { Ship, Gameboard };
--- scripts.test.js
+++ scripts.test.js
@@ -1,18 +1,26 @@
-let { Ship } = require("./scripts");
+let { Ship, Gameboard } = require("./scripts");
 
 test('correctly creates a hit array', () => {
     let ship = new Ship(5);
     expect(ship.hitArray).toStrictEqual([
         false, false, false, false, false
     ]);
+    ship = new Ship(3);
+    expect(ship.hitArray).toStrictEqual([
+        false, false, false
+    ]);
 });
 
 test('correctly hits the right spots in a ship', () => {
     let ship = new Ship(5);
-    ship.hit(3)
+    ship.hit(3);
     expect(ship.hitArray).toStrictEqual([
         false, false, false, true, false
     ]);
+    ship.hit(0);
+    expect(ship.hitArray).toStrictEqual([
+        true, false, false, true, false
+    ])
 });
 
 test('correctly calculates whether or not a ship has been sunk', () => {
@@ -22,4 +30,36 @@ test('correctly calculates whether or not a ship has been sunk', () => {
         ship.hit(i);
     }
     expect(ship.isSunk()).toBe(true);
+});
+
+test.skip('correctly adds ships to the ship array', () => {
+    let gameboard = new Gameboard();
+    gameboard.placeShip(5, 4, "v", 5);
+    expect(gameboard.ships[0]).toEqual({
+        ship: new Ship(5),
+        x: 5,
+        y: [4, 5, 6, 7, 8],
+        or: "v"
+    });
+    gameboard.placeShip(2, 3, "h", 3);
+    expect(gameboard.ships[1]).toEqual({
+        ship: new Ship(3),
+        x: [2, 3, 4],
+        y: 3,
+        or: "h"
+    });
+});
+
+test('correctly handles an attack that hits a ship', () => {
+    let gameboard = new Gameboard();
+    gameboard.placeShip(5, 4, "v", 5);
+    gameboard.receiveAttack(5, 5);
+    expect(gameboard.ships[0].ship.hitArray).toEqual([
+        false, true, false, false, false
+    ])
+    gameboard.placeShip(3, 3, "h", 3);
+    gameboard.receiveAttack(4, 3);
+    expect(gameboard.ships[1].ship.hitArray).toEqual([
+        false, true, false
+    ]);
 });
