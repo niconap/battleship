a5c47e70b3dbd9f98658e694b454fc542932cbbd
Nico Nap <nico.nap@outlook.com>
2021-05-07 12:32:01 +0200

    Refactoring

--- scripts.js
+++ scripts.js
@@ -17,18 +17,7 @@ function Ship(length) {
   // that spot has been hit
   this.hit = function(spot) {
     this.hitArray[spot] = true;
-    this.isSunk();
-  }
-
-  // See whether or not the ship has been sunk based on the hitArray
-  this.isSunk = function() {
-    this.sunk = this.hitArray.every(element => {
-      if (element) {
-        return true;
-      } else {
-        return false;
-      }
-    });
+    this.sunk = this.hitArray.every(Boolean);
   }
 }
 
@@ -73,21 +62,13 @@ function Gameboard() {
         } else if (or == "h") {
           let foundX = x.includes(ship.x);
           let foundY = ship.y.includes(y);
-          if (foundX && foundY) {
-            return true; 
-          } else {
-            return false;
-          }
+          return foundX && foundY;
         }
       } else if (ship.or == "h") {
         if (or == "v") {
           let foundX = ship.x.includes(x);
           let foundY = y.includes(ship.y);
-          if (foundX && foundY) {
-            return true; 
-          } else {
-            return false;
-          }
+          return foundX && foundY;
         } else if (or == "h" && ship.y == y) {
           let found = ship.x.some(coord => x.indexOf(coord) >= 0);
           return found;
@@ -130,11 +111,7 @@ function Gameboard() {
   // array
   this.allSunk = function() {
     return this.ships.every(element => {
-      if (element.ship.sunk) {
-        return true;
-      } else {
-        return false;
-      }
+      return element.ship.sunk;
     })
   }
 
@@ -220,7 +197,7 @@ function Gameboard() {
             clicked = true;
             current = false;
             checkSunk();
-            gameloop();
+            switchPlayers();
           }
         })
       }
@@ -257,7 +234,6 @@ function Gameboard() {
         x = 0;
         y++
       }
-
     }
   }
 }
@@ -392,7 +368,7 @@ function initialize() {
   computerBoard.render("computerboard");
 }
 
-function gameloop() {
+function switchPlayers() {
   if (!gameover) {
     if (current) {
       return;
@@ -405,8 +381,9 @@ function gameloop() {
   }
 }
 
-function drag(e, length) {
-  e.dataTransfer.setData("number", length);
+// Drag and drop functions for ship placement
+function drag(event, length) {
+  event.dataTransfer.setData("number", length);
   let vertical = document.getElementById("vertical");
   let horizontal = document.getElementById("horizontal");
   let orientation;
@@ -415,18 +392,18 @@ function drag(e, length) {
   } else if (horizontal.checked) {
     orientation = "h";
   }
-  e.dataTransfer.setData("orientation", orientation)
-  e.dataTransfer.setData("id", e.target.id);
+  event.dataTransfer.setData("orientation", orientation)
+  event.dataTransfer.setData("id", event.target.id);
 }
 
-function allowDrop(e) {
-  e.preventDefault();
+function allowDrop(event) {
+  event.preventDefault();
 }
 
-function drop(e, x, y) {
-  e.preventDefault();
-  let orientation = e.dataTransfer.getData("orientation");
-  let data = e.dataTransfer.getData("number");
+function drop(event, x, y) {
+  event.preventDefault();
+  let orientation = event.dataTransfer.getData("orientation");
+  let data = event.dataTransfer.getData("number");
   let xco = x;
   let yco = y;
 
@@ -445,7 +422,7 @@ function drop(e, x, y) {
   if (!playerBoard.checkBoard(x, y, orientation, data) && checkPossibleCoords(xco, yco, orientation, data)) {;
     playerBoard.placeShip(xco, yco, orientation, parseInt(data));
     playerBoard.render("playerboard");
-    let template = document.getElementById(e.dataTransfer.getData("id"));
+    let template = document.getElementById(event.dataTransfer.getData("id"));
     let container = document.getElementById("choices");
     container.removeChild(template);
   }
@@ -469,23 +446,15 @@ function checkPossibleCoords(x, y, or, length) {
   if (or == "v") {
     possibleX = spots;
     possibleY = spots.slice(0, 11 - length);
-    if (possibleY.indexOf(y) < 0) {
-      return false;
-    } else {
-      return true;
-    }
+    return !(possibleY.indexOf(y) < 0);
   } else if (or == "h") {
     possibleY = spots;
     possibleX = spots.slice(0, 11 - length);
-    if (possibleX.indexOf(x) < 0) {
-      return false;
-    } else {
-      return true;
-    }
+    return !(possibleX.indexOf(x) < 0);
   }
 }
 
 initialize();
-gameloop();
+switchPlayers();
 
 module.exports = { Ship, Gameboard, Player };
